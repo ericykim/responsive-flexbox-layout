8647c92c3747d194952460f0b56cc169438ecf46
Eric Kim <ericykim5399@gmail.com>
2023-03-02 18:23:57 -0500

    first commit

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'responsive-flexbox-layout'
+import 'responsive-flexbox-layout/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
